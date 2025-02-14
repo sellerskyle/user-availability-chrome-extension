79c727df53d67ee6000cd8b909fc37269fb3f3bd
sellerskyle <sellerskyle@live.com>
2025-02-14 07:17:05 -0600

    store characteristic instead of device

--- page.js
+++ page.js
@@ -1,27 +1,38 @@
-let device = null;
+let characteristic = null;
 
 let options = {
   filters: [{ services: ["4fafc201-1fb5-459e-8fcc-c5c9c331914b"] }],
 };
 
-document.querySelector("button").onclick = () =>
-  navigator.bluetooth
-    .requestDevice(options)
-    .then((incoming) => {
-      //   device.setDevice(incoming);
-      //   window.device = incoming;
-      //   updateDevice(incoming);
-      device = incoming;
+document.querySelector("#connection").onclick = () => {
+  if (!characteristic) {
+    navigator.bluetooth
+      .requestDevice(options)
+      .then(async (incoming) => {
+        let device = incoming;
+        let server = await device.gatt.connect();
+        let service = await server.getPrimaryService(
+          "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+        );
+        characteristic = await service.getCharacteristic(
+          "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+        );
 
-      console.log(`Name: ${incoming.name}`);
-      // Do something with the device.
-    })
-    .catch((error) => console.error(`Something went wrong. ${error}`));
+        document.querySelector(
+          "#connection"
+        ).textContent = `Disconnect from ${incoming.name}`;
+        document.querySelector("#status").style.backgroundColor = "black";
+        console.log(document.querySelector("#status"));
+      })
+      .catch((error) => console.error(`Something went wrong. ${error}`));
+  } else {
+    document.querySelector("#connection").textContent = "Connect";
+    document.querySelector("#status").style.backgroundColor = "black";
+    characteristic.service.device.gatt.disconnect();
+  }
+};
 
 document.querySelector("#write").onclick = async () => {
-  //   const dev = device;
-  //   const dev = device.getDevice();
-  console.log(dev);
   await updateStatus();
 };
 
@@ -33,23 +44,16 @@ navigator.serviceWorker.addEventListener("message", async (event) => {
 });
 
 const updateStatus = async () => {
-  if (!!device) {
-    const server = await device.gatt.connect();
-    console.log(server);
-    const service = await server.getPrimaryService(
-      "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-    );
-    console.log(service);
-    const characteristic = await service.getCharacteristic(
-      "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-    );
-    console.log(characteristic);
+  if (!!characteristic) {
     const currentTabs = await chrome.tabs.query({});
 
     const isCurrentlyInMeeting =
       currentTabs?.some(
         (t) => t.url.includes("meet.google.com") && t.title.includes("Meet -")
       ) ?? false;
+
+    document.querySelector("#status").style.backgroundColor =
+      isCurrentlyInMeeting ? "red" : "green";
 
     const val = new Uint8Array(1);
     val[0] = isCurrentlyInMeeting ? 1 : 0;
--- popup.js
+++ popup.js
@@ -1,2 +1,11 @@
 //TODO: if tab exists, pull it up instead
-chrome.tabs.create({ url: "page.html" });
+const currentTabs = await chrome.tabs.query({});
+
+const tab = currentTabs.find((t) => t.title === "User Availability");
+console.log(currentTabs, tab);
+if (!!tab) {
+  await chrome.tabs.update(tab.id, { active: true });
+  await chrome.windows.update(tab.windowId, { focused: true });
+} else {
+  chrome.tabs.create({ url: "page.html" });
+}
